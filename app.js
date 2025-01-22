3aa0592588f13f892be5f06728bedf323816da62
KristttoferBruno <kristtoferbruno77@gmail.com>
2025-01-22 16:23:17 -0400

    projeto inicial

---
+++ app.js
@@ -0,0 +1,25 @@
+alert('Boas vindas ao jogo do número do secreto');
+let numeroMaximo = 32;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute = 0;
+let tentativas = 1;
+
+while (chute != numeroSecreto){
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    if (numeroSecreto == chute){
+        break;
+    }
+    else{
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute} / tentativa ${tentativas}`);
+        }
+        else{
+            alert(`O número secreto é maior que ${chute} / tentativa ${tentativas}`);
+        }
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} e acertou na ${tentativas} ${palavraTentativa}`);
